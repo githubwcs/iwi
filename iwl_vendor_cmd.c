7f2f32bc828cf53bc990d6769686d994aa8a58c3
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2016-03-22 14:13:22 +0200

    iw: add support for LQM events
    Change-Id: I28764874dcff26dcae3df67b412b2ef075f8ea71
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/25134
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Berg, Johannes <johannes.berg@intel.com>

--- iwl_vendor_cmd.c
+++ iwl_vendor_cmd.c
@@ -21,6 +21,11 @@ static struct nla_policy iwl_vendor_policy[NUM_IWL_MVM_VENDOR_ATTR] = {
 	[IWL_MVM_VENDOR_ATTR_LL] = { .type = NLA_U8 },
 	[IWL_MVM_VENDOR_ATTR_VIF_LOAD] = { .type = NLA_U8 },
 	[IWL_MVM_VENDOR_ATTR_LOAD] = { .type = NLA_U8 },
+	[IWL_MVM_VENDOR_ATTR_WIPHY_FREQ] = { .type = NLA_U32 },
+	[IWL_MVM_VENDOR_ATTR_CHANNEL_WIDTH] = { .type = NLA_U32 },
+	[IWL_MVM_VENDOR_ATTR_CENTER_FREQ1] = { .type = NLA_U32 },
+	[IWL_MVM_VENDOR_ATTR_CENTER_FREQ2] = { .type = NLA_U32 },
+	[IWL_MVM_VENDOR_ATTR_LQM_RESULT] = { .type = NLA_NESTED },
 };
 
 static int handle_iwl_vendor_dev_tx_power(struct nl80211_state *state,
@@ -160,6 +165,111 @@ static void parse_tcm_event(struct nlattr *data)
 	}
 }
 
+static int handle_iwl_vendor_start_lqm(struct nl80211_state *state,
+				       struct nl_msg *msg, int argc,
+				       char **argv, enum id_input id)
+{
+	struct nlattr *limits;
+	unsigned long val;
+	char *end;
+
+	if (argc != 2)
+		return 1;
+
+	NLA_PUT_U32(msg, NL80211_ATTR_VENDOR_ID, INTEL_OUI);
+	NLA_PUT_U32(msg, NL80211_ATTR_VENDOR_SUBCMD,
+		    IWL_MVM_VENDOR_CMD_QUALITY_MEASUREMENTS);
+
+	limits = nla_nest_start(msg, NL80211_ATTR_VENDOR_DATA);
+	if (!limits)
+		return -ENOBUFS;
+
+	val = strtoul(argv[0], &end, 10);
+	if (end == argv[0] || *end)
+		return 1;
+	NLA_PUT_U32(msg, IWL_MVM_VENDOR_ATTR_LQM_DURATION, val);
+
+	val = strtoul(argv[1], &end, 10);
+	if (end == argv[1] || *end)
+		return 1;
+	NLA_PUT_U32(msg, IWL_MVM_VENDOR_ATTR_LQM_TIMEOUT, val);
+
+	nla_nest_end(msg, limits);
+	return 0;
+
+nla_put_failure:
+	return -ENOBUFS;
+}
+COMMAND(iwl, lqm, "duration(us) timeout(us)", NL80211_CMD_VENDOR, 0,
+	CIB_NETDEV, handle_iwl_vendor_start_lqm, "");
+
+static const char const* width2str[] =
+{
+	[NL80211_CHAN_WIDTH_20_NOHT] = "20noHT",
+	[NL80211_CHAN_WIDTH_20] = "HT20",
+	[NL80211_CHAN_WIDTH_40] = "HT40",
+	[NL80211_CHAN_WIDTH_80] = "VHT80",
+	[NL80211_CHAN_WIDTH_80P80] = "VHT80+80",
+	[NL80211_CHAN_WIDTH_160] = "VHT160",
+};
+
+static const char const* lqmstatus2str[] =
+{
+	[IWL_MVM_VENDOR_LQM_STATUS_SUCCESS] = "Success",
+	[IWL_MVM_VENDOR_LQM_STATUS_TIMEOUT] = "Timeout",
+	[IWL_MVM_VENDOR_LQM_STATUS_ABORT] = "Abort",
+};
+
+static void parse_lqm_event(struct nlattr *data)
+{
+	struct nlattr *lqm[NUM_IWL_MVM_VENDOR_LQM_RESULT];
+	struct nlattr *attrs[NUM_IWL_MVM_VENDOR_ATTR];
+	struct nlattr *sta_air_time;
+	static struct nla_policy lqm_policy[] = {
+		[IWL_MVM_VENDOR_ATTR_LQM_ACTIVE_STA_AIR_TIME] = { .type = NLA_NESTED },
+		[IWL_MVM_VENDOR_ATTR_LQM_OTHER_STA] = { .type = NLA_U32 },
+		[IWL_MVM_VENDOR_ATTR_LQM_MEAS_TIME] = { .type = NLA_U32 },
+		[IWL_MVM_VENDOR_ATTR_LQM_RETRY_LIMIT] = { .type = NLA_U32 },
+		[IWL_MVM_VENDOR_ATTR_LQM_MEAS_STATUS] = { .type = NLA_U32 },
+	};
+	int rem, i;
+
+	if (nla_parse_nested(attrs, MAX_IWL_MVM_VENDOR_ATTR, data, iwl_vendor_policy) ||
+	    !attrs[IWL_MVM_VENDOR_ATTR_LQM_RESULT] ||
+	    !attrs[IWL_MVM_VENDOR_ATTR_WIPHY_FREQ] ||
+	    !attrs[IWL_MVM_VENDOR_ATTR_CHANNEL_WIDTH] ||
+	    !attrs[IWL_MVM_VENDOR_ATTR_CENTER_FREQ1]) {
+		printf(" Ignore invalid LQM data");
+		return;
+	}
+
+	printf(" ==> Intel LQM event: freq %dHz Width: %s",
+	       nla_get_u32(attrs[IWL_MVM_VENDOR_ATTR_CENTER_FREQ1]),
+	       width2str[nla_get_u32(attrs[IWL_MVM_VENDOR_ATTR_CHANNEL_WIDTH])]);
+
+	if (nla_parse_nested(lqm, MAX_IWL_MVM_VENDOR_LQM_RESULT,
+	    attrs[IWL_MVM_VENDOR_ATTR_LQM_RESULT], lqm_policy))
+		printf("Bad internal data");
+
+	if (!lqm[IWL_MVM_VENDOR_ATTR_LQM_ACTIVE_STA_AIR_TIME] ||
+	    !lqm[IWL_MVM_VENDOR_ATTR_LQM_OTHER_STA] ||
+	    !lqm[IWL_MVM_VENDOR_ATTR_LQM_MEAS_TIME] ||
+	    !lqm[IWL_MVM_VENDOR_ATTR_LQM_RETRY_LIMIT] ||
+	    !lqm[IWL_MVM_VENDOR_ATTR_LQM_MEAS_STATUS])
+		printf("Bad internal data");
+
+	printf(" status: %s\n",
+	       lqmstatus2str[nla_get_u32(lqm[IWL_MVM_VENDOR_ATTR_LQM_MEAS_STATUS])]);
+
+	printf("\tMeasurement time: %dus\n", nla_get_u32(lqm[IWL_MVM_VENDOR_ATTR_LQM_MEAS_TIME]));
+	printf("\tother stas: %dus\n", nla_get_u32(lqm[IWL_MVM_VENDOR_ATTR_LQM_OTHER_STA]));
+	printf("\tretry limit: %d frames\n", nla_get_u32(lqm[IWL_MVM_VENDOR_ATTR_LQM_RETRY_LIMIT]));
+
+	i = 0;
+	nla_for_each_nested(sta_air_time, lqm[IWL_MVM_VENDOR_ATTR_LQM_ACTIVE_STA_AIR_TIME], rem)
+		printf("\tSTA[%d]: %dus\n", i++, nla_get_u32(sta_air_time));
+}
+
 void iwl_parse_event(__u32 vendor_id, struct nlattr **attrs)
 {
 	if (vendor_id != INTEL_OUI)
@@ -169,8 +279,10 @@ void iwl_parse_event(__u32 vendor_id, struct nlattr **attrs)
 	case IWL_MVM_VENDOR_CMD_TCM_EVENT:
 		parse_tcm_event(attrs[NL80211_ATTR_VENDOR_DATA]);
 		break;
+	case IWL_MVM_VENDOR_CMD_QUALITY_MEASUREMENTS:
+		parse_lqm_event(attrs[NL80211_ATTR_VENDOR_DATA]);
+		break;
 	default:
 		break;
 	}
 }
-
