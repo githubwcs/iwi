ea405b16cb3fa753877b400a1968d9c79962fb7d
Ilan Peer <ilan.peer@intel.com>
2016-11-24 13:57:35 +0200

    iw: nan2: Beautify security prints in match event
    Change-Id: Ibaddd9fbc658959c89b2141797273baeb157a59f
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>

--- event.c
+++ event.c
@@ -427,12 +427,15 @@ static void parse_nan_sec(struct nlattr *sec)
 	if (!n_pmkids)
 		goto out;
 
-	printf(". pmkids:");
-	for (i = 0; i < n_pmkids; i++)
-		iw_hexdump(" ", &pmkids[i * NL80211_NAN_PMKID_LEN],
-			   NL80211_NAN_PMKID_LEN);
+	for (i = 0; i < n_pmkids; i++) {
+		size_t j;
+
+		printf(", pmkid %d: ", i);
+		for (j = 0; j < NL80211_NAN_PMKID_LEN; j++)
+			printf("%02x ", pmkids[(i * NL80211_NAN_PMKID_LEN) + j]);
+	}
 out:
-	printf(".\n");
+	printf("\n");
 }
 
 static void parse_nan_match(struct nlattr **attrs)
