c8186b4ac5c5117788f925881b58ba2272648f59
Karthik D A <karthik.d.a@intel.com>
2021-02-18 13:58:27 +0530

    iw: Add support to handle msmt evnt
    Add support to handle time sync measurement
    vendor event
    
    type=feature
    ticket=jira:WIFI-83270
    
    Change-Id: Id294368fa6dbf6d4a0d6fa54fe9be12174b9b948
    Signed-off-by: Karthik D A <karthik.d.a@intel.com>

--- iwl_vendor_cmd.c
+++ iwl_vendor_cmd.c
@@ -1072,3 +1072,66 @@ static void parse_neighbor_report(unsigned int id, unsigned int subcmd, struct n
 }
 
 VENDOR_EVENT(INTEL_OUI, IWL_MVM_VENDOR_CMD_NEIGHBOR_REPORT_RESPONSE, parse_neighbor_report);
+
+static void parse_time_sync_msmt_event(unsigned int id,
+				       unsigned int subcmd,
+				       struct nlattr *data)
+{
+	int err;
+	struct nlattr *tb[MAX_IWL_MVM_VENDOR_ATTR + 1];
+
+	err = nla_parse_nested(tb, MAX_IWL_MVM_VENDOR_ATTR, data, NULL);
+	if (err) {
+		printf(" Invalid time sync msmt event");
+		return;
+	}
+
+	if (tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_T1])
+		printf("\n\ttime_sync_t1: %lu\n",
+		       nla_get_u64(tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_T1]));
+
+        if (tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_T2])
+		printf("\ttime_sync_t2: %lu\n",
+		       nla_get_u64(tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_T2]));
+
+	if (tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_T3])
+		printf("\ttime_sync_t3: %lu\n",
+		       nla_get_u64(tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_T3]));
+
+	if (tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_T4])
+		printf("\ttime_sync_t4: %lu\n",
+		       nla_get_u64(tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_T4]));
+
+	if (tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_DIALOG_TOKEN])
+		printf("\tdialog_token: %d\n",
+		       nla_get_u32(tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_DIALOG_TOKEN]));
+
+	if (tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_FUP_DIALOG_TOKEN])
+		printf("\tfup_dialog_token: %d\n",
+		       nla_get_u32(tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_FUP_DIALOG_TOKEN]));
+
+	if (tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_T1_MAX_ERROR])
+		printf("\tt1_error: %d\n",
+		       nla_get_u32(tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_T1_MAX_ERROR]));
+
+	if (tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_T2_MAX_ERROR])
+		printf("\tt2_error: %d\n",
+		       nla_get_u32(tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_T2_MAX_ERROR]));
+
+	if (tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_T3_MAX_ERROR])
+		printf("\tt3_error: %d\n",
+		       nla_get_u32(tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_T3_MAX_ERROR]));
+
+	if (tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_T4_MAX_ERROR])
+		printf("\tt4_error: %d\n",
+		       nla_get_u32(tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_T4_MAX_ERROR]));
+
+	if (tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_VS_DATA]) {
+		iw_hexdump("\tVendor_data",
+			   nla_data(tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_VS_DATA]),
+			   nla_len(tb[IWL_MVM_VENDOR_ATTR_TIME_SYNC_VS_DATA]));
+	}
+}
+
+VENDOR_EVENT(INTEL_OUI, IWL_MVM_VENDOR_CMD_TIME_SYNC_MSMT_EVENT,
+	     parse_time_sync_msmt_event);
